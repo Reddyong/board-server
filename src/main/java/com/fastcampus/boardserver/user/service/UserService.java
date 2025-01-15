d21a81837245d88781da84cb24b4ced9d584a6a4
Reddyong <d0209hong@naver.com>
2025-01-15 15:55:30 +0900

    #3 - UserService, UserServiceImpl 기본 틀 생성.

---
+++ src/main/java/com/fastcampus/boardserver/user/service/UserService.java
@@ -0,0 +1,18 @@
+package com.fastcampus.boardserver.user.service;
+
+import com.fastcampus.boardserver.user.dto.UserDTO;
+
+public interface UserService {
+    // 회원가입
+    void register(UserDTO userDTO);
+
+    // 로그인
+
+    // 중복 ID 검증
+
+    // 패스워드 변경
+
+    // 유저 정보 조회
+
+    // 회원 탈퇴
+}
---
+++ src/main/java/com/fastcampus/boardserver/user/service/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.fastcampus.boardserver.user.service;
+
+import com.fastcampus.boardserver.user.dto.UserDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Override
+    public void register(UserDTO userDTO) {
+
+    }
+}
