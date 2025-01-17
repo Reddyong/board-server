0c5a083aa888abe1cabd4a2bc47ee8de353a0784
Reddyong <d0209hong@naver.com>
2025-01-17 17:12:15 +0900

    #3 - 회원 정보 조회 메서드 쿼리 추가.
    * 회원의 아이디와 탈퇴여부를 통한 조회.

--- src/main/java/com/fastcampus/boardserver/user/repository/UserRepository.java
+++ src/main/java/com/fastcampus/boardserver/user/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.fastcampus.boardserver.user.repository;
 import com.fastcampus.boardserver.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByUserId(String userId);
+
+    Optional<User> findByUserIdAndAndIsWithDraw(String userId, boolean isWithdraw);
 }
