5995c3e482ff12c6c3d2bcc1ffb5b62ae56050b2
Reddyong <d0209hong@naver.com>
2025-01-17 16:26:40 +0900

    #3 - 중복 아이디 검증을 위한 exists 메서드 쿼리 생성.

--- src/main/java/com/fastcampus/boardserver/user/repository/UserRepository.java
+++ src/main/java/com/fastcampus/boardserver/user/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.fastcampus.boardserver.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    boolean existsByUserId(String userId);
 }
