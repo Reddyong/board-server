6a8b0a9c252abab3af00dd7b41de22032721e190
Reddyong <d0209hong@naver.com>
2025-01-17 17:10:24 +0900

    #3 - 로그인 기능 컨트롤러 메서드 작성.
    * 로그인 성공 시 user dto 반환.
    * 실패 시 각 검증에 해당하는 예외 발생.

--- src/main/java/com/fastcampus/boardserver/controller/UserController.java
+++ src/main/java/com/fastcampus/boardserver/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.fastcampus.boardserver.controller;
 
+import com.fastcampus.boardserver.user.dto.UserDTO;
+import com.fastcampus.boardserver.user.dto.request.LoginProfile;
 import com.fastcampus.boardserver.user.dto.request.UserProfile;
 import com.fastcampus.boardserver.user.service.UserServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +21,20 @@ public class UserController {
     private final UserServiceImpl userService;
 
     @PostMapping(path = "/sign-up")
-    public ResponseEntity<?> register(UserProfile userProfile) {
+    public ResponseEntity<?> register(
+            @Valid @RequestBody UserProfile userProfile
+    ) {
         userService.register(userProfile);
 
         return ResponseEntity.ok("register success");
+    }
+
+    @PostMapping(path = "/sign-in")
+    public ResponseEntity<?> login(
+            @Valid @RequestBody LoginProfile loginProfile
+    ) {
+        UserDTO loginedUserDTO = userService.login(loginProfile);
+
+        return ResponseEntity.ok(loginedUserDTO);
     }
 }
